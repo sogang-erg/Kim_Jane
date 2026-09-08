92026590c3a81f943ba7573ca474535d48ac72bd
ianekem0 <92069774+ianekem0@users.noreply.github.com>
2026-09-08 15:03:18 +0900

    섹션3

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Debug.Log(Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // [SerializeField]
+    // string playerName;
+    // [SerializeField]
+    // int health;
+    // [SerializeField]
+    // int attack;
+
+    public string playerName = "Player";
+    public int health = 100;
+    public int attack = 10;
+    float sumTime = 0;
+    public Player target; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        sumTime += Time.deltaTime;
+        if (sumTime >= 1)
+        {
+            sumTime = 0;
+            // GenerateHP();
+            Attack();
+        }
+    }
+
+    // void GenerateHP()
+    // {
+    //     this.health++;
+    // }
+
+        void Attack()
+    {
+        if (target != null)
+        {
+            target.health -= attack;
+        }
+    }
+}
