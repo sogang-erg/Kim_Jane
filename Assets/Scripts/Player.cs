290d60651a666cf1fe0c4640ef0e66c33a4149aa
ianekem0 <92069774+ianekem0@users.noreply.github.com>
2026-09-14 17:37:41 +0900

    섹션6

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,47 +2,53 @@ using UnityEngine;
 
 public class Player : Character
 {
-    // [SerializeField]
-    // string playerName;
-    // [SerializeField]
-    // int health;
-    // [SerializeField]
-    // int attack;
-
-    // public string playerName = "Player";
-    // public int health = 100;
-    // public int attack = 10;
-    // float sumTime = 0;
-    // public Player target; 
+    public float speed = 5; // 언제든 수정 가능하도록 설정
+    Transform monster;
 
     void Start()
     {
-        // gameObject.GetComponent<Transform>().position = new Vector3(10, 10, 10);
-        // GetComponent<Transform>().position = new Vector3(10, 10, 10);
-        // transform.position = new Vector3(10, 10, 10); // 동일 코드
+        // transform.position += new Vector3(1, 1, 1); // Vector3 벡터 통째로 변환, 덧셈 가능
+        monster = GameObject.Find("Monster").transform; // 몬스터 찾기
     }
 
     void Update()
     {
-        // sumTime += Time.deltaTime;
-        // if (sumTime >= 1)
-        // {
-        //     sumTime = 0;
-        //     // GenerateHP();
-        //     Attack();
+        // Vector3 position = transform.position; // 통채로 복붙
+        // // position.z += 1; // 1씩 증가-하드 코딩,,
+        // position.z += Time.deltaTime * speed; // 이게 나음
+        // transform.position = position; // 다시 갖다 놓음
+
+        // transform.position += new Vector3(0, 0, 1) * speed * Time.deltaTime; // z 이동
+        // transform.position += Vector3.forward * speed * Time.deltaTime; // z 이동(위와 동일)
+
+        // // Vector(AB) = B - A
+        // Vector3 dir = monster.position - transform.position; 
+        // float distance = dir.magnitude; // .magnitude: 벡터 크기 반환 기능
+        // if (distance < 5)
+        // { 
+        //     Debug.Log("Attack!"); // 공격!!
         // }
+        // else
+        // { 
+        //     transform.position += dir.normalized * speed * Time.deltaTime; 
+        //     transform.LookAt(monster); // 바라보기 회전
+        // }
+
+        // dir.Normalize(); // 원본 그대로 가져와서 변환 방법
+        // Vector3 dirNormalized = dir.normalized; // .normalized: 벡터 방향 반환 기능
+        // transform.position += dir.normalized * speed * Time.deltaTime; // 이동
+
+        // 키보드로 이동
+        Vector3 dir = Vector3.zero; // 누적 설정
+        if (Input.GetKey(KeyCode.W))
+            dir += Vector3.forward; 
+        if (Input.GetKey(KeyCode.S))
+            dir += Vector3.back; 
+        if (Input.GetKey(KeyCode.A))
+            dir += Vector3.left; 
+        if (Input.GetKey(KeyCode.D))
+            dir += Vector3.right; 
+        transform.position += dir.normalized * speed * Time.deltaTime; // 정규화로 길이 날리기
+        // transform.Translate(dir.normalized * speed * Time.deltaTime); // 이 방법으로도 구현하곤 함
     }
-
-    // void GenerateHP()
-    // {
-    //     this.health++;
-    // }
-
-    //     void Attack()
-    // {
-    //     if (target != null)
-    //     {
-    //         target.health -= attack;
-    //     }
-    // }
 }
