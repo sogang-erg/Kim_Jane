2c0b2f5587909c8fa2bb272832a6003f09c98ca5
ianekem0 <92069774+ianekem0@users.noreply.github.com>
2026-09-15 12:30:44 +0900

    섹션7 New1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,43 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    private InputAction _testAction; 
 
     void Start()
     {
+        _testAction = InputSystem.actions.FindAction("Player/Test"); // 액션 가져오기
+
+        if (_testAction == null)
+        {
+            Debug.LogError("Test Action을 찾지 못했습니다.");
+            return;
+        }
+
+        _testAction.Enable(); // 활성화
+        Debug.Log("Test Action을 찾았습니다.");
+
+        _testAction.performed += OnPerformed; // 이벤트 방식으로 전달해 받음
+        _testAction.canceled += OnCancelled; // 이벤트 방식으로 전달해 받음
     }
 
-    void Update()
+    void OnPerformed(InputAction.CallbackContext context)
+    { 
+        Debug.Log("Performed"); 
+    }
+
+    void OnCancelled(InputAction.CallbackContext context)
+    { 
+        Debug.Log("Cancelled"); 
+    }
+
+    void Update() // polling 방식
     {
-        // if (Input.GetKeyDown(KeyCode.Space)) // 처음으로 눌러짐
+        // if (_testAction.WasPerformedThisFrame()) // GetKeyDown에 해당
         // {
-        //     Debug.Log("GetKeyDown"); 
+        //     Debug.Log("Performed");
         // }
 
-        // if (Input.GetKey(KeyCode.Space)) // 계속 누름
+        // if (_testAction.IsPressed()) // GetKey에 해당
         // {
-        //     Debug.Log("GetKey"); 
+        //     Debug.Log("Pressed");
         // }
 
-        // if (Input.GetKeyUp(KeyCode.Space)) // 누르지 않게 됨
+        // if (_testAction.WasReleasedThisFrame()) // GetKeyUp에 해당
         // {
-        //     Debug.Log("GetKeyUp"); 
+        //     Debug.Log("Released");
         // }
-
-        // // Input Manager 축 활용
-        // float v = Input.GetAxis("Vertical");
-        // float h = Input.GetAxis("Horizontal");
-        // Debug.Log($"Vertical {v}");
-        // Debug.Log($"Horizontal {h}");
-
-        // jump, Fire1 
-        if (Input.GetButtonDown("Jump"))
-        { 
-            Debug.Log("Jump");
-        }
-        if (Input.GetButtonDown("Fire1"))
-        { 
-            Debug.Log("Fire1");
-        }
     }
 }
