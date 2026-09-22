95320aeb504daa50cc10b90f39f590a922522f78
ianekem0 <92069774+ianekem0@users.noreply.github.com>
2026-09-22 22:37:52 +0900

    섹션9

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,133 +1,45 @@
 using UnityEngine;
-using UnityEngine.AI;
-
-// Collider(IsTrigger X): Collision 용도
-// Collider(IsTrigger O): Trigger 용도
-// Rigidbody(IsKinematic X): 물리적 영향을 받음 (힘, 중력, 충돌 등)
-// Rigidbody(IsKinematic O): 물리적 영향을 받지 않음 (인형을 알아서 움직이는 용도)
-
-// Case Study 1: Player vs Wall
-// A: Collider(IsTrigger X) + Rigidbody(IsKinematic X)
-// B: Collider(IsTrigger X)
-
-// Case Study 2: Player vs Portal
-// A: Collider(IsTrigger ?) + Rigidbody(IsKinematic ?)
-// B: Collider(IsTrigger O)
-
-// Case Study 3: Player vs Monster (MMO) << 벽 충돌 없음, 유닛 간 충돌 없음
-// A: Collider(IsTrigger X) + Rigidbody(IsKinematic O)
-// B: Collider(IsTrigger X) + Rigidbody(IsKinematic O)
-
-// Case Study 4: Player vs Monster (Vampire Survival) 
-// A: Collider(IsTrigger X) + Rigidbody(IsKinematic X)
-// B: Collider(IsTrigger X) + Rigidbody(IsKinematic X)
 
 public class Player : MonoBehaviour
 {
-    private float _searchRadius = 5f;
-    private float _searchTimer = 0f;
-    private float _searchInterval = 1f;
+    Animator animator;
 
-    private Rigidbody _rb;
+    int IDLE = Animator.StringToHash("IDLE"); // 해쉬 값
+    int MOVE = Animator.StringToHash("MOVE");
+    int ATTACK = Animator.StringToHash("ATTACK");
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>(); 
+        animator = GetComponent<Animator>();
     }
 
     // Shape Casting
     void Update()
     {
-        _searchTimer += Time.deltaTime;
-        if (_searchTimer >= _searchInterval)
+        if (Input.GetKey(KeyCode.F1))
+            // animator.Play("IDLE"); // string 문자류는 기본적으로 느림
+            // animator.Play(IDLE); // 해쉬 값 사용
+            animator.SetBool("moving", false); // 파라미터 연동
+
+        if (Input.GetKey(KeyCode.F2))
+            // animator.Play("MOVE");
+            // animator.Play(MOVE); // 해쉬 값 사용
+            animator.SetBool("moving", true); // 파라미터 연동
+
+        if (Input.GetKey(KeyCode.F3))
         {
-            _searchTimer = 0f;
-            SearchNearbyColliders();
-            PickObject();
+            AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0); // Layer 상태 정보
+            // if (info.IsName ("ATTACK") == false || info.normalizedTime >= 1) // 애니메이션 재생 끝
+            //     animator.Play("ATTACK", 0, 0); // 애니메이션 이름 기입, Layer번호, 재생옵션(-1:다시안틂, 0:리셋, 0.5f중간, 1:?)
+            if (info.shortNameHash == ATTACK == false || info.normalizedTime >= 1) // 애니메이션 재생 끝
+                // animator.Play(ATTACK, 0, 0); // 해쉬 값 사용
+                animator.SetTrigger("attack"); // 파라미터 연동
         }
     }
 
-    // 구로 주변 물체 탐지
-    void SearchNearbyColliders()
+    public void TriggerOnDamaged()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, _searchRadius);
-
-        foreach (Collider col in colliders)
-        {
-            if (col.transform != gameObject)
-            Debug.Log($"Nearby Collider: {col.gameObject.name}");
-        }
+        Debug.Log("Triggered On Damaged");
     }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, _searchRadius);
-    }
-
-    // RayCast 발동
-    void PickObject()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
-        {
-            Debug.Log($"Picked Object: {hit.collider.gameObject.name}");
-        }
-        else
-        {
-            Debug.Log("No object picked");
-        }
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 dir = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.W))
-            dir += Vector3.forward;
-        if (Input.GetKey(KeyCode.S))
-            dir += Vector3.back;
-        if (Input.GetKey(KeyCode.A))
-            dir += Vector3.left;
-        if (Input.GetKey(KeyCode.D))
-            dir += Vector3.right;
-
-        if (_rb != null)
-            _rb.MovePosition(transform.position + dir.normalized * Time.deltaTime * 5f);
-
-    }
-
-    // 충돌 이벤트
-    public void OnCollisionEnter(Collision collision) // 시작
-    {
-        Debug.Log($"OnCollisionEnter({collision.gameObject.name})");
-    }
-
-    public void OnCollisionStay(Collision collision) // 진행 
-    {
-        Debug.Log($"OnCollisionStay({collision.gameObject.name})");
-    }
-
-    public void OnCollisionExit(Collision collision) // 끝
-    {
-        Debug.Log($"OnCollisionExit({collision.gameObject.name})");
-    }
-
-    // 트리거 이벤트
-    public void OnTriggerEnter(Collider other) // 시작
-    {
-        Debug.Log($"OnTriggerEnter({other.gameObject.name})");
-    }
-
-    public void OnTriggerStay(Collider other) // 진행
-    {
-        Debug.Log($"OnTriggerStay({other.gameObject.name})");
-    }
-
-    public void OnTriggerExit(Collider other) // 끝
-    {
-        Debug.Log($"OnTriggerExit({other.gameObject.name})");
-    }
+    
 }
